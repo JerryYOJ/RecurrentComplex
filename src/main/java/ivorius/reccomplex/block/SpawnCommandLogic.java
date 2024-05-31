03d977eb9ebe3ad735448b413dd4af7e4c7db6c4
jerry <jerry@test.com>
2024-05-31 18:27:45 +0800

    Modified to make SpawnCommand work on Mohist

--- src/main/java/ivorius/reccomplex/block/SpawnCommandLogic.java
+++ src/main/java/ivorius/reccomplex/block/SpawnCommandLogic.java
@@ -10,6 +10,7 @@ import net.minecraft.command.*;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.CrashReportCategory;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.CommandBlockBaseLogic;
 import net.minecraft.util.*;
 import ivorius.reccomplex.RCConfig;
 import ivorius.reccomplex.utils.accessor.RCAccessorCommandBase;
@@ -25,104 +26,16 @@ import java.text.SimpleDateFormat;
 /**
  * Created by lukas on 11.02.15.
  */
-public abstract class SpawnCommandLogic implements ICommandSender
+public abstract class SpawnCommandLogic extends CommandBlockBaseLogic
 {
     /** The formatting for the timestamp on commands run. */
     private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("HH:mm:ss");
-    /** The command stored in the command block. */
-    private String commandStored = "";
-    /** The custom name of the command block. (defaults to "@") */
-    private String customName = "@";
     private final CommandResultStats resultStats = new CommandResultStats();
-
-    public NBTTagCompound writeToNBT(NBTTagCompound p_189510_1_)
-    {
-        p_189510_1_.setString("Command", this.commandStored);
-        p_189510_1_.setString("CustomName", this.customName);
-
-        this.resultStats.writeStatsToNBT(p_189510_1_);
-        return p_189510_1_;
-    }
-
-    public void readDataFromNBT(NBTTagCompound nbt)
-    {
-        this.commandStored = nbt.getString("Command");
-
-        if (nbt.hasKey("CustomName", 8))
-        {
-            this.customName = nbt.getString("CustomName");
-        }
-
-        this.resultStats.readStatsFromNBT(nbt);
-    }
-
-    @Override
-    public boolean canUseCommand(int permLevel, String commandName)
-    {
-        return permLevel <= 2;
-    }
-
-    public void setCommand(String command)
-    {
-        this.commandStored = command;
-    }
-
-    public String getCommand()
-    {
-        return this.commandStored;
-    }
-
-    public void trigger(World worldIn)
-    {
-        if (!worldIn.isRemote)
-        {
-            MinecraftServer minecraftserver = this.getServer();
-
-            if (minecraftserver != null && minecraftserver.isAnvilFileSet() && minecraftserver.isCommandBlockEnabled())
-            {
-                ICommandManager icommandmanager = minecraftserver.getCommandManager();
-
-                ICommandListener cachedAdmin = null;
-                if (!RCConfig.notifyAdminOnBlockCommands)
-                {
-                    cachedAdmin = RCAccessorCommandBase.getCommandAdmin();
-                    CommandBase.setCommandListener(null);
-                }
-
-                try
-                {
-                    icommandmanager.executeCommand(this, this.commandStored);
-                }
-                catch (Exception ex)
-                {
-                    CrashReport crashreport = CrashReport.makeCrashReport(ex, "Executing command block");
-                    CrashReportCategory crashreportcategory = crashreport.makeCategory("Command to be executed");
-                    crashreportcategory.addDetail("Command", this::getCommand);
-                    crashreportcategory.addDetail("Name", this::getName);
-                    throw new ReportedException(crashreport);
-                }
-
-                if (!RCConfig.notifyAdminOnBlockCommands)
-                    CommandBase.setCommandListener(cachedAdmin);
-            }
-        }
-    }
-
-    @Override
-    public String getName()
-    {
-        return this.customName;
-    }
 
     @Override
     public ITextComponent getDisplayName()
     {
         return new TextComponentString(this.getName());
-    }
-
-    public void setName(String name)
-    {
-        this.customName = name;
     }
 
     @Override
